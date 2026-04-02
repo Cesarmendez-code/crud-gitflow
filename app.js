5907b46b417da5d553630d74436bfb49abe096da
Cesarmendez-code <cesarmendez2124@gmail.com>
2026-04-01 22:42:14 -0400

    prueba

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 function readUsers() {
     console.log("Mostrando usuarios");
 }
+function updateUser() {
+    console.log("Actualizando usuario");
+}
+function deleteUser() {
+    console.log("Eliminando usuario");
+}
+console.log("Error corregido");
+console.log("prueba");
